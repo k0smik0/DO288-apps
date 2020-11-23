bafcf757f62aa689827704f3f4a031394e9df5cf
Student User <student@workstation.lab.example.com>
2020-11-23 09:26:19 -0500

    lab 2.4 script

---
+++ bin/_lab2.4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cp -vaf container-build/Dockerfile.unprivileged container-build/Dockerfile
+
+oc new-app --as-deployment-config --name hola \
+https://github.com/${RHT_OCP4_GITHUB_USER}/DO288-apps#container-build \
+--context-dir container-build
