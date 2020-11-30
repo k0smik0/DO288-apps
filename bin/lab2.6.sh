d055adf7c6915215f601164e73e32a386b87bb7c
Student User <student@workstation.lab.example.com>
2020-11-30 10:15:03 -0500

    up

--- bin/lab2.6.sh
+++ bin/lab2.6.sh
@@ -20,15 +20,18 @@ oc get route ; pause
 
 curl http://myapp-${RHT_OCP4_DEV_USER}-app-config.${RHT_OCP4_WILDCARD_DOMAIN} ; pause
 
+echo 4.1;
 oc create configmap myappconf --from-literal APP_MSG="Test Message" ; pause
 
+echo 4.2;
 oc describe cm/myappconf ; pause
 
+echo 4.4 ;
 oc create secret generic myappfilesec --from-file /home/student/DO288-apps/app-config/myapp.sec ; pause
 
 oc get secret/myappfilesec -o json ; pause
 
-echo "5"
+echo 5;
 oc set env dc/myapp --from configmap/myappconf ; pause
 
 # also from /home/student/DO288/labs/app-config/inject-secret-file.sh
@@ -36,3 +39,28 @@ oc set volume dc/myapp --add -t secret -m /opt/app-root/secure --name myappsec-v
 
 echo "6.1"
 oc status ; pause
+
+echo 6.2 ;
+oc get pods ; pause ;
+
+echo 6.3 ;
+running_pod=$(oc get pods | grep Running | awk '{print $1}') ; oc rsh ${running_pod} env | grep APP_MSG ; pause
+
+echo 6.4 ;
+curl http://myapp-${RHT_OCP4_DEV_USER}-app-config.${RHT_OCP4_WILDCARD_DOMAIN} ; pause
+
+echo 7.1 ;
+oc edit cm/myappconf ; pause
+
+echo 7.2 ;
+oc describe cm/myappconf ; pause
+
+echo 7.3 ;
+oc rollout latest dc/myapp ; pause
+
+echo 7.4 ; 
+oc get pods ; pause
+
+echo 7.5; 
+curl http://myapp-${RHT_OCP4_DEV_USER}-app-config.${RHT_OCP4_WILDCARD_DOMAIN} ; pause
+
