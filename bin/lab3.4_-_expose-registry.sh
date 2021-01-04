3bcc80c3522b57bf39ac8548f75a2f4184aba507
Student User <student@workstation.lab.example.com>
2021-01-04 13:12:35 -0500

    added 3.2 and 3.4

---
+++ bin/lab3.4_-_expose-registry.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+source /usr/local/etc/ocp4.config 
+source $HOME/DO288-apps/bin/_pause.sh
+source $HOME/DO288-apps/bin/_main.sh
+source $HOME/DO288-apps/bin/_oc_get_pods_last_running.sh
+
+
+
+
+function __1() {
+	lab expose-registry start
+	oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+	oc get route -n openshift-image-registry
+	INTERNAL_REGISTRY=$( oc get route default-route -n openshift-image-registry -o jsonpath='{.spec.host}' )
+	echo ${INTERNAL_REGISTRY}
+}
+
+function __2() {
+sudo podman login -u ${RHT_OCP4_QUAY_USER} quay.io
+pause
+sudo podman run -d --name sleep quay.io/${RHT_OCP4_QUAY_USER}/ubi-sleep:1.0
+pause
+sudo podman ps
+pause
+sudo podman logs sleep
+pause
+sudo podman stop sleep
+pause
+sudo podman rm sleep
+}
+
+function __3() {
+oc login -u ${RHT_OCP4_DEV_USER} -p ${RHT_OCP4_DEV_PASSWORD} ${RHT_OCP4_MASTER_API}
+pause
+oc new-project ${RHT_OCP4_DEV_USER}-external-registry
+pause
+oc new-app --as-deployment-config --name sleep --docker-image quay.io/${RHT_OCP4_QUAY_USER}/ubi-sleep:1.0
+pause
+oc create secret generic quayio --from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json --type kubernetes.io/dockerconfigjson
+pause
+oc secrets link default quayio --for pull
+pause
+oc new-app --as-deployment-config --name sleep --docker-image quay.io/${RHT_OCP4_QUAY_USER}/ubi-sleep:1.0
+pause
+oc get pods
+pause
+pod=$(oc_get_pods_last_running); oc logs $pod
+}
+
+function __4() {
+oc delete project ${RHT_OCP4_DEV_USER}-external-registry
+pause
+skopeo delete docker://quay.io/${RHT_OCP4_QUAY_USER}/ubi-sleep:1.0
+}
+
+
+
+[ $# -lt 1 ] && _help && exit 1
+
+_execute $1
